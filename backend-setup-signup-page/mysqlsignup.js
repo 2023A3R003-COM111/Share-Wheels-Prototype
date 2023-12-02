0b5e4c02e4d0752a8af6ab1c9a36913f4f313c59
Saksham Singh Jamwal <145137116+MajXin@users.noreply.github.com>
2023-12-02 22:38:53 +0530

    added files for signup database connection

---
+++ backend-setup-signup-page/mysqlsignup.js
@@ -0,0 +1,37 @@
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+
+//     host: "localhost",
+//     user: "saksham",
+//     password: "",
+//     database: "db"
+// });
+
+// ///connect to the database
+// connection.connect((err) => {
+//     if (err) {
+//       console.error('Error connecting to database:', err);
+//       return;
+//     }
+//     console.log('Connected to database');
+//   });
+
+const mysql = require('mysql');
+
+// Create a connection to the MySQL server
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'megtvnklsw',
+  database: 'db',
+});
+
+// Connect to the MySQL server
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL: ' + err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as id ' + connection.threadId);
+}); 
